24713a3f0f70406e9e8a2b338fb3d290ff305a18
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-08-07 20:39:12 +0300

    addedn story to empty table component

--- src/stories/Table.stories.ts
+++ src/stories/Table.stories.ts
@@ -7,12 +7,10 @@ const meta = {
     tags: ["autodocs"],
     argTypes: {
         headers: {
-            description: "Array of headers. Can contain arrays of strings or JSX elements",
-            control: { type: "array" },
+            description: "Array of headers. Contains arrays of strings or JSX elements",
         },
         data: {
-            description: "Array of data. Can contain arrays of strings or JSX elements",
-            control: { type: "array" },
+            description: "Array of data. Contains arrays of strings or JSX elements",
         },
     },
 } satisfies Meta<typeof Table>;
@@ -20,10 +18,20 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-/** The component is used to draw data in a convenient table form  */
+const commonHeaders = ["Header 1", "Header 2", "Header 3", "Header 4", "Header 5", "Header 6"];
+
+/** The Table component is used to draw data in a convenient table form  */
 export const Primary: Story = {
     args: {
-        headers: ["Header 1", "Header 2", "Header 3", "Header 4", "Header 5", "Header 6"],
+        headers: commonHeaders,
         data: [["Cell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5", "Cell 6"]],
+    },
+};
+
+/** The empty Table component which displaying when there is no data  */
+export const Empty: Story = {
+    args: {
+        headers: commonHeaders,
+        data: [],
     },
 };
